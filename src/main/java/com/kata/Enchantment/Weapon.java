42d225da9f4073d75048461c948c06ec400146ae
MrBrambleberry <bamburyn@gmail.com>
2021-04-06 20:47:22 +0100

    [NB] Add ability for a weapon to have an enchantment

--- src/main/java/com/kata/Enchantment/Weapon.java
+++ src/main/java/com/kata/Enchantment/Weapon.java
@@ -1,10 +1,13 @@
 package com.kata.Enchantment;
 
+import com.kata.Enchantment.enums.Enchantment;
+
 public class Weapon {
     private String name;
     private int minimumAttackDamage;
     private int maximumAttackDamage;
     private double attackSpeed;
+    private Enchantment enchantment;
 
     Weapon(String name) {
         this.name = name;
@@ -13,7 +16,21 @@ public class Weapon {
         this.attackSpeed = 1.2;
     }
 
+    Weapon(String name, Enchantment enchantment) {
+        this.name = name;
+        this.minimumAttackDamage = 5;
+        this.maximumAttackDamage = 10;
+        this.attackSpeed = 1.2;
+        this.enchantment = enchantment;
+    }
+
     public String describe() {
+        if (this.enchantment != null) {
+            return this.enchantment.getPrefix() + " " + this.name + "\r\n" + this.minimumAttackDamage + " - "
+                    + this.maximumAttackDamage + " attack damage" + "\r\n" + this.attackSpeed + " attack speed" + "\r\n"
+                    + this.enchantment.getAttributes();
+        }
+
         return this.name + "\r\n" + this.minimumAttackDamage + " - " + this.maximumAttackDamage + " attack damage"
                 + "\r\n" + this.attackSpeed + " attack speed";
 
--- src/test/java/com/kata/Enchantment/WeaponTest.java
+++ src/test/java/com/kata/Enchantment/WeaponTest.java
@@ -3,11 +3,20 @@ package com.kata.Enchantment;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.kata.Enchantment.enums.Enchantment;
+
 public class WeaponTest {
 
     @Test
     void the_weapon_provides_an_expected_description() {
         Weapon weapon = new Weapon("Dagger of the Nooblet");
         assertEquals("Dagger of the Nooblet\r\n5 - 10 attack damage\r\n1.2 attack speed", weapon.describe());
+    }
+
+    @Test
+    void if_given_an_enchantment_the_expected_description_is_printed() {
+        Weapon weapon = new Weapon("Dagger of the Nooblet", Enchantment.FIRE);
+        assertEquals("Inferno Dagger of the Nooblet\r\n5 - 10 attack damage\r\n1.2 attack speed\r\n+5 fire damage",
+                weapon.describe());
     }
 }
